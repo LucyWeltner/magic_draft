c74425562c18cf80baff9b1b5c0ee83b0b014107
LucyWeltner <lucyiweltner@gmail.com>
2020-04-16 16:57:01 -0400

    Users can now draft from three different sets of packs; website automatically ends draft once the user drafts cards from all packs.

--- src/components/pack_container.js
+++ src/components/pack_container.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {nextPack} from '../actions.js'
 import Card from './card.js'
+import {getPacks} from '../actions.js'
 
 class PackContainer extends React.Component {
+	state= {packs_counter: 0}
 	render() {
 		console.log(this.props.packs)
-		console.log(this.props.turn)
-		return (
-			<div>
-				<p>Pack Goes Here</p>
-				<div id="cards">{this.props.current_pack.map((card, index) => <Card key={index} card={card}/>)}</div>
-				<button onClick={() => this.props.nextPack()} >Next Pack</button>
-			</div>
-		)
+		if (this.props.current_pack.length > 0 && this.state.packs_counter < 3) {
+			return (
+				<div>
+					<p>Pack Goes Here</p>
+					<div id="cards">{this.props.current_pack.map((card, index) => <Card key={index} card={card}/>)}</div>
+					<p></p>
+				</div>
+			)
+		}
+		else {
+			return (
+				<div>
+					{(this.state.packs_counter < 2) ? (<p><button onClick = {() => {this.props.getPacks(); this.state.packs_counter += 1}}>Next Set of Packs</button></p>) : (<p>You're Done!</p>)}
+				</div>
+			)
+		}
 	}
 }
 
@@ -21,8 +30,8 @@ const getPropsFromState = (state) => {
 	return {current_pack: state.current_pack, turn: state.turn, packs: state.packs}
 }
 
-const mapDispatchToState = dispatch => {
-	return {nextPack: () => dispatch(nextPack())}
+const getPropsFromDispatch = (dispatch) => {
+	return {getPacks: () => dispatch(getPacks())}
 }
 
-export default connect(getPropsFromState, mapDispatchToState)(PackContainer)
+export default connect(getPropsFromState, getPropsFromDispatch)(PackContainer)
