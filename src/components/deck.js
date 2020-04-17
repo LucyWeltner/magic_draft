f7ed1234eb5bfe2ca9a4dfc109ad5970ea9d3c22
LucyWeltner <lucyiweltner@gmail.com>
2020-04-17 15:19:49 -0400

    Adds deck component which displays the name, comments, and cards of an individual deck

---
+++ src/components/deck.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Deck extends React.Component {
+	render() {
+		return (
+			<div>
+			<h3>Name of Deck: {this.props.deck.name}</h3>
+			<p><b>Comments:</b> {this.props.deck.comments}</p>
+			<h3>Cards:</h3>
+			{this.props.deck.cards.map(card => <img src={card.image_url}/>)}
+			</div>
+		)
+	}
+}
--- src/components/decks_container.js
+++ src/components/decks_container.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import Deck from './deck.js'
 
 export default class DecksContainer extends React.Component {
 	state = {
 		decks: []
 	}
 	componentDidMount() {
-		fetch('localhost:3000/decks').then(response => reponse.json())
-		.then(decks => this.setState({decks}, () => console.log(state)))
+		fetch('http://localhost:3000/decks').then(response => response.json())
+		.then(decks => this.setState({decks}, () => console.log(this.state)))
 	}
 	render() {
 		return (
 			<React.Fragment>
 				<h2>Decks Go Here</h2>
-				<{this.state.decks.map(deck => <Deck key={deck.id} deck={deck}/>)}
+				{this.state.decks.map(deck => <Deck key={deck.id} deck={deck}/>)}
 			</React.Fragment>
 		)
 	}
