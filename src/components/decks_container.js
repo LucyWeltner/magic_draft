98aa49a66d1a9692b80cb7949f4b9f8cce2bc327
LucyWeltner <lucyiweltner@gmail.com>
2020-04-17 15:12:03 -0400

    Fetches and displays decks in deck container component

--- src/components/decks_container.js
+++ src/components/decks_container.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 export default class DecksContainer extends React.Component {
+	state = {
+		decks: []
+	}
+	componentDidMount() {
+		fetch('localhost:3000/decks').then(response => reponse.json())
+		.then(decks => this.setState({decks}, () => console.log(state)))
+	}
 	render() {
 		return (
-			<h2>Decks Go Here</h2>
+			<React.Fragment>
+				<h2>Decks Go Here</h2>
+				<{this.state.decks.map(deck => <Deck key={deck.id} deck={deck}/>)}
+			</React.Fragment>
 		)
 	}
 }
